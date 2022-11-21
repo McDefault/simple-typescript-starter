115d25618821d1835538b711d783d303d7811279
Max Ottenhof <max.ottenhof@gmail.com>
2022-11-21 21:38:49 +0100

    index file sapphire client

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+import { SapphireClient } from '@sapphire/framework';
+import {Intents} from "discord.js";
+
+const client = new SapphireClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+client.login(process.env.TOKEN);
